369579a4b7e2b875f101ae63dfd147ed6c78bb16
Aaron Bui <s3740934@rmit.edu.vn>
2021-05-21 18:15:31 +0700

    update

--- src/main/resources/static/js/parse.js
+++ src/main/resources/static/js/parse.js
@@ -1,15 +1,12 @@
 function parse_url() {
     var url = document.location.href,
-        params,
-        data = {},
-        tmp;
-
-    if (url.split("?")[1] !== undefined) {
-        params = url.split("?")[1].split("&")
-        for (var i = 0, l = params.length; i < l; i++) {
-            tmp = params[i].split("=");
-            data[tmp[0]] = tmp[1];
-        }
-        return data;
+      params = url.split("?")[1].split("&"),
+      data = {},
+      tmp;
+    for (var i = 0, l = params.length; i < l; i++) {
+      tmp = params[i].split("=");
+      data[tmp[0]] = tmp[1];
     }
-}
+  
+    return data;
+  }
--- src/main/resources/static/js/unsplash.js
+++ src/main/resources/static/js/unsplash.js
@@ -3,117 +3,103 @@ var unsplash_API = "https://api.unsplash.com/";
 var number_of_image = 10;
 
 async function fetch_api(type, query, page_num, handler) {
-    var command, api;
-    switch (type) {
+  var command, api;
+  switch (type) {
+    case "random":
+      command = "photos/random?count=";
+      api = unsplash_API + command + query + "&" + access_key;
+      break;
+    case "search":
+      command = "search/photos?query=";
+      api = unsplash_API + command + query  + "&" + access_key;
+      break;
+    case "get_image":
+      command = "photos/";
+      api = unsplash_API + command + query + "?" + access_key;
+
+      break;
+  }
+  // console.log(page_num)
+  if (page_num !== undefined) {
+    api = api + "&page=" + page_num;
+  }
+
+  fetch(api)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      switch (type) {
         case "random":
-            command = "photos/random?count=";
-            api = unsplash_API + command + query + "&" + access_key;
-            break;
+          insert_image(json, number_of_image);
+          break;
         case "search":
-            command = "search/photos?query=";
-            api = unsplash_API + command + query + "&" + access_key;
-            break;
+          if (page_num > json.total_pages)
+            console.log("total pages", json.total_pages);
+          else
+            insert_image(json.results, number_of_image);
+          break;
         case "get_image":
-            command = "photos/";
-            api = unsplash_API + command + query + "?" + access_key;
+          load_info(json);
+          break;
+      }
+    });
 
-            break;
-    }
-    // console.log(page_num)
-    if (page_num !== undefined) {
-        api = api + "&page=" + page_num;
-    }
-
-    fetch(api)
-        .then((response) => response.json())
-        .then((json) => {
-            console.log(json);
-            switch (type) {
-                case "random":
-                    insert_image(json, number_of_image);
-                    break;
-                case "search":
-                    if (page_num > json.total_pages)
-                        console.log("total pages", json.total_pages);
-                    else
-                        insert_image(json.results, number_of_image);
-                    break;
-                case "get_image":
-                    load_info(json);
-                    break;
-            }
-        });
-
-    if (handler !== undefined) {
-        handler.flag = 0;
-    }
+  if (handler !== undefined) {
+    handler.flag = 0;
+  }
 }
 
 function insert_image(results, frame) {
-    for (let index = 0; index < results.length && index < frame; index++) {
-        var result = results[index];
-        var post_url = "/pic" + "?id=" + encodeURIComponent(result.id);
-        var img_html =
-            `<div class="bg-image hover-overlay" id="mask" style="margin-bottom: 0.4rem">` +
-            `<img src="${result.urls.regular}" class="w-100" alt="${result.alt_description}"  class="img_fluid">` +
-            `<a href="${post_url}" target="_blank"><div class="mask" style="background-color: rgba(0, 0, 0, 0.2);"></div></a></div>`
-        document.getElementById("gallery").innerHTML
-            += img_html;
-        // console.log(img_html);
-    }
+  for (let index = 0; index < results.length && index < frame; index++) {
+    var result = results[index];
+    var post_url = "/pic" + "?id=" + encodeURIComponent(result.id);
+    var img_html =
+      `<div class="bg-image hover-overlay" id="mask" style="margin-bottom: 0.4rem">` +
+      `<img src="${result.urls.regular}" class="w-100" alt="${result.alt_description}"  class="img_fluid">` +
+      `<a href="${post_url}" target="_blank"><div class="mask" style="background-color: rgba(0, 0, 0, 0.2);"></div></a></div>`
+    document.getElementById("gallery").innerHTML 
+    += img_html;
+    // console.log(img_html);
+  }
 }
 
 function load_info(post) {
-    var description = post.description;
-    var location = post.location.name;
-    if (description == null) {
-        description = post.alt_description;
-    }
-    if (location == null) {
-        location = "Unknown";
-    }
+  // var description = post.description;
+  var location = post.location.name;
+  // if (description == null) {
+  //   description = post.alt_description;
+  // }
+  if (location == null) {
+    location = "Unknown";
+  }
 
-    document.querySelector("#image").src = post.urls.regular;
-    document.querySelector("#description").innerHTML += description + "<br/>";
-    document.querySelector("#location").innerHTML += "<br/>" + location + "<br/>";
-    document.querySelector("#view").innerHTML += "<br/>" + post.views + "<br/>";
-    document.querySelector("#download").innerHTML += "<br/>" + post.downloads + "<br/>";
+  document.querySelector("#image").src = post.urls.regular;
+  // document.querySelector("#description").innerHTML += description + "<br/>";
+  document.querySelector("#location").innerHTML += "<br/>" + location + "<br/>";
+  document.querySelector("#view").innerHTML += "<br/>" + post.views + "<br/>";
+  document.querySelector("#download").innerHTML += "<br/>" + post.downloads + "<br/>";
+  var button = document.getElementById("download_button");
+  var save_button = document.getElementById("save_button");
+  
+  // Download button
+  button.addEventListener('click', async function (e) {
+      const picture = await fetch(post.urls.raw)
+      const picBlog = await picture.blob()
+      const pictureURL = URL.createObjectURL(picBlog)
+    
+      const link = document.createElement('a')
+      link.href = pictureURL;
+      link.download = post.id;
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+      console.log("Download button clicked")
+    })
 
-    var button = document.getElementById("download_button");
-    var description_button = document.getElementById("show_description");
-    var save_btn = document.getElementById("save_button");
-
-    // Download button
-    button.addEventListener('click', async function (e) {
-        const picture = await fetch(post.urls.raw)
-        const picBlog = await picture.blob()
-        const pictureURL = URL.createObjectURL(picBlog)
-
-        const link = document.createElement('a')
-        link.href = pictureURL;
-        link.download = post.id;
-        document.body.appendChild(link)
-        link.click()
-        document.body.removeChild(link)
-        console.log("Download button clicked")
-    });
-
-    // View Description button
-    description_button.addEventListener('click', function (e) {
-        console.log("Description button clicked")
-        var cont = document.getElementById("description-container");
-        if (cont.style.display === "none") {
-            cont.style.display = "block";
-        } else
-            cont.style.display = "none";
-    });
-
-    // Save to Collection button
-    save_btn.addEventListener('click', function (e) {
-        save_btn.style.display = "none";
-        document.getElementById("sb-container").style.display = "block";
-        console.log("Save button clicked")
-    });
+  save_button.addEventListener('click', function (e) {
+    save_button.style.display ="none";
+    document.getElementById("sb-container").style.display = "block";
+  })
 }
 
 
